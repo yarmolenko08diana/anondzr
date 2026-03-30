cda29bb6e49ec45f631d74c3acf011563219d266
Diana <yarmolenko08diana@gmail.com>
2026-03-31 00:49:15 +0500

    main

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-REPLY_MAP_FILE = os.getenv("REPLY_MAP_FILE", "reply_map.json")
+REPLY_MAP_FILE = os.getenv("REPLY_MAP_FILE")
 
 bot = Bot(
     token=TOKEN,
